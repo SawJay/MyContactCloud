3e33fbe38ab540aa716a6f31d65d63fba9800639
SawJay <sawyerjd1@gmail.com>
2024-05-10 12:58:26 -0400

    Fixed Edit contact

--- MyContactCloud.Client/Services/Interfaces/IContactDTOService.cs
+++ MyContactCloud.Client/Services/Interfaces/IContactDTOService.cs
@@ -6,5 +6,7 @@ namespace MyContactCloud.Client.Services.Interfaces
     {
         Task<ContactDTO> CreateContactAsync(ContactDTO contactDTO, string userId);
         Task<IEnumerable<ContactDTO>> GetContactsAsync(string userId);
+        Task UpdateContactAsync(ContactDTO contactDTO, string userId);
+        Task<ContactDTO?> GetContactByIdAsync(int contactId, string userId);
     }
 }
